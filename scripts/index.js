ef2388538d8b649788f3245c49cf66e4529a3f39
Ekaterina Nechkina <e.nechkina@yandex.ru>
2022-07-15 16:41:33 +0300

    add feature for add card

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,17 @@ const popupAddPlace  = document.querySelector('.popup_add-place');
 const popupAddButtonClose = document.querySelector('div.popup_add-place .popup__button_input_close');
 
 // Находим форму в DOM
-const formElement = document.querySelector('.popup__input');
+const formElementEdit = document.querySelector('div.popup_edit-profile .popup__input');
+const formElementAddPlace = document.querySelector('div.popup_add-place .popup__input');
+
 // Находим поля формы в DOM
 const nameInput = document.querySelector('.popup__input_data_name');
 const jobInput = document.querySelector('.popup__input_data_description');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__description');
+
+const placeInput = document.querySelector('.popup__input_data_place');
+const liknInput = document.querySelector('.popup__input_data_link');
 
 //Задается лист для элементов и область для тепмлейтов
 const elementList = document.querySelector('.element');
@@ -20,27 +25,27 @@ const elementTemplate = document.querySelector('.element_template').content;
 
 const initialCards = [
   {
-    name: 'Архыз',
+    place: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-    name: 'Челябинская область',
+    place: 'Челябинская область',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-    name: 'Иваново',
+    place: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-    name: 'Камчатка',
+    place: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-    name: 'Холмогорский район',
+    place: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-    name: 'Байкал',
+    place: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
@@ -48,13 +53,15 @@ const initialCards = [
 
 initialCards.forEach (function(element) {
   const newElement = elementTemplate.cloneNode(true);
-
-  newElement.querySelector('.element__title').textContent = element.name;
-  newElement.querySelector('.element__photo').alt = element.name;
+  newElement.querySelector('.element__title').textContent = element.place;
+  newElement.querySelector('.element__photo').alt = element.place;
   newElement.querySelector('.element__photo').src = element.link;
   elementList.append(newElement);
 });
 
+const renderItems = function(placeInput,liknInput ) {
+  elementList.insertAdjacentElement('afterbegin', (element.place, element.link));
+}
 
 function openPopupEditProfile() {
   nameInput.value = profileName.textContent;
@@ -74,26 +81,23 @@ function closePopupPlaceAdd() {
   popupAddPlace.classList.remove('popup_opened');
 }
 
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-
-    closePopupEditProfile();
+function formSubmitHandlerEdit (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopupEditProfile()
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+function formSubmitHandlerAddPlace (evt) {
+  evt.preventDefault();
+  const newElement = elementTemplate.cloneNode(true);
+  newElement.querySelector('.element__title').textContent = placeInput.value;
+  newElement.querySelector('.element__photo').alt = placeInput.value;
+  newElement.querySelector('.element__photo').src = liknInput.value;
+  elementList.prepend(newElement);
 
+  closePopupPlaceAdd();
+}
 
 profileEditButton.addEventListener('click', openPopupEditProfile);
 popupEditButtonClose.addEventListener('click', closePopupEditProfile);
@@ -101,5 +105,7 @@ popupEditButtonClose.addEventListener('click', closePopupEditProfile);
 placeAddButton.addEventListener('click', openPopupPlaceAdd);
 popupAddButtonClose.addEventListener('click', closePopupPlaceAdd);
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElementEdit.addEventListener('submit', formSubmitHandlerEdit);
+formElementAddPlace.addEventListener('submit', formSubmitHandlerAddPlace);
+
 
