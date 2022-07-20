aafcb0876e40f9eb7af932ac1b17629c9d1bcd71
Ekaterina Nechkina <e.nechkina@yandex.ru>
2022-07-20 13:18:06 +0300

    fix for 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,8 @@
 const profileEditButton = document.querySelector('.profile__edit');
 const popupEditProfile = document.querySelector('.popup_edit_profile');
-const popupEditButtonClose = document.querySelector('div.popup_edit_profile .popup__button_input_close');
 
 const placeAddButton = document.querySelector('.profile__add-button');
 const popupAddPlace  = document.querySelector('.popup_add_place');
-const popupAddButtonClose = document.querySelector('div.popup_add_place .popup__button_input_close');
 const formElementEdit = document.querySelector('div.popup_edit_profile .popup__input');
 const formElementAddPlace = document.querySelector('div.popup_add_place .popup__input');
 const popupZoomPhoto = document.querySelector('.popup_zoom_image');
@@ -15,14 +13,14 @@ const nameInput = document.querySelector('.popup__input_data_name');
 
 const jobInput = document.querySelector('.popup__input_data_description');
 const placeInput = document.querySelector('.popup__input_data_place');
-const liknInput = document.querySelector('.popup__input_data_link');
+const linkInput = document.querySelector('.popup__input_data_link');
 
 const elementList = document.querySelector('.element');
 const elementTemplate = document.querySelector('.element_template').content;
 
 const popupPhoto = document.querySelector('.popup__photo');
 const popupImageText = document.querySelector('.popup__description');
-const popupPhotoButtonClose = document.querySelector('div.popup_zoom_image .popup__button_input_close');
+const closeButtons = document.querySelectorAll('.popup__button_input_close');
 
 const initialCards = [
   {
@@ -68,86 +66,75 @@ function createElement(element) {
     item.remove();
   });
 
-  photoPlace.addEventListener('click', function openPhotoPlace() {
-    popupZoomPhoto.classList.add('popup_opened');
-
+    photoPlace.addEventListener('click', function() {
     popupPhoto.src = photoPlace.closest('.element__photo').src;
     popupImageText.textContent = photoPlace.closest('.element__photo').alt;
+    popupPhoto.alt = photoPlace.closest('.element__photo').alt;
+    openPopup(popupZoomPhoto);
+    });
+
+  return newElement;
+}
+
+function render() {
+  initialCards.forEach(newElement => {
+    const card = createElement(newElement);
+    elementList.append(card);
   });
+}
+render();
 
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+  //данные для попаgа профиля
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 
-
-  elementList.append(newElement);
+  //данные для попапа добавить места
+    placeInput.value = '';
+    linkInput.value = '';
 }
 
-initialCards.forEach(createElement);
-
-function openPopupEditProfile() {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-  popupEditProfile.classList.add('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-function closePopupEditProfile() {
-  popupEditProfile.classList.remove('popup_opened');
-}
-
-function openPopupPlaceAdd() {
-  popupAddPlace.classList.add('popup_opened');
-}
-
-function closePopupPlaceAdd() {
-  popupAddPlace.classList.remove('popup_opened');
-}
-
- function closePopupZoomPhoto() {
-  popupZoomPhoto.classList.remove('popup_opened');
-};
-
-function formSubmitHandlerEdit (evt) {
+function handleProfileFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopupEditProfile()
+
+  closePopup(popupEditProfile)
 }
 
-function formSubmitHandlerAddPlace (evt) {
+function handleubmitHandlerAddPlace (evt) {
   evt.preventDefault();
-  const newElement = elementTemplate.cloneNode(true);
-  newElement.querySelector('.element__title').textContent = placeInput.value;
-  newElement.querySelector('.element__photo').alt = placeInput.value;
-  newElement.querySelector('.element__photo').src = liknInput.value;
-  const likeButton = newElement.querySelector('.element__like');
-  const deleteButton = newElement.querySelector('.element__delete');
-  const photoPlace = newElement.querySelector('.element__photo');
+  const NewCard = {
+    place: placeInput.value,
+    link: linkInput.value
+  }
+  const card = createElement(NewCard);
+  elementList.prepend(card);
 
-
-  likeButton.addEventListener('click', function() {
-    likeButton.classList.toggle('element__like_active');
-  });
-
-  deleteButton.addEventListener('click', function() {
-    const item = deleteButton.closest('.element__item');
-    item.remove();
-  });
-
-  photoPlace.addEventListener('click', function openPhotoPlace() {
-    popupZoomPhoto.classList.add('popup_opened');
-
-    popupPhoto.src = photoPlace.closest('.element__photo').src;
-    popupImageText.textContent = photoPlace.closest('.element__photo').alt;
-  });
-
-
-  elementList.prepend(newElement);
-
-  closePopupPlaceAdd();
+  closePopup(popupAddPlace);
 }
 
-profileEditButton.addEventListener('click', openPopupEditProfile);
-popupEditButtonClose.addEventListener('click', closePopupEditProfile);
-popupPhotoButtonClose.addEventListener('click', closePopupZoomPhoto);
-placeAddButton.addEventListener('click', openPopupPlaceAdd);
-popupAddButtonClose.addEventListener('click', closePopupPlaceAdd);
-formElementEdit.addEventListener('submit', formSubmitHandlerEdit);
-formElementAddPlace.addEventListener('submit', formSubmitHandlerAddPlace);
+profileEditButton.addEventListener('click', function() {
+  openPopup(popupEditProfile);
+});
+placeAddButton.addEventListener('click', function() {
+  openPopup(popupAddPlace);
+});
+
+closeButtons.forEach((button) => {
+  // находим 1 раз ближайший к крестику попап
+  const popup = button.closest('.popup');
+  // устанавливаем обработчик закрытия на крестик
+  button.addEventListener('click', () => closePopup(popup));
+});
+
+popupEditProfile.addEventListener('click', function() {
+   closePopup(popupAddPlace);
+  });
+formElementEdit.addEventListener('submit', handleProfileFormSubmit);
+formElementAddPlace.addEventListener('submit', handleubmitHandlerAddPlace);
